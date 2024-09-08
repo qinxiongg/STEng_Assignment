9cca0c98f53111009dcb6374c7d45bd486fe5c95
Qin Xiong <tanq0046@e.ntu.edu.sg>
2024-09-08 13:07:49 +0800

    1st commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,54 @@
+<script>
+</script>
+
+<div class="login-container">
+	<h1>LOGIN</h1>
+	<div class="login">
+		<form method="POST" action="?/login">
+			<label for="uname"></label>
+			<input type="text" placeholder="Username" name="uname" required />
+
+			<label for="psw"></label>
+			<input type="password" placeholder="Password" name="psw" required />
+
+			<button type="submit"><b>LOGIN</b></button>
+		</form>
+	</div>
+</div>
+
+<style>
+	.login-container {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		min-height: 100vh;
+		flex-direction: column;
+		margin: 0;
+	}
+
+	.login {
+		text-align: center;
+		width: 25%;
+	}
+
+	input {
+		display: block;
+		width: 100%;
+		margin-bottom: 20px;
+		padding: 15px;
+		box-sizing: border-box;
+		border: lightgray;
+		border-radius: 5px;
+		background-color: lightgray;
+	}
+
+	button {
+		padding: 15px;
+		border-radius: 5px;
+		border: none;
+		color: white;
+		cursor: pointer;
+		background-color: black;
+		width: 100%;
+	}
+</style>
---
+++ src/routes/tms/+page.svelte
@@ -0,0 +1,16 @@
+<script></script>
+
+<nav>
+	<ul>
+		<li>Hello, User</li>
+		<li>
+			<a href="/applications">Applications</a>
+		</li>
+        <li>
+            <a href="/usermanagement">User Management</a>
+        </li>
+        <li>
+            <a href="/editprofile">Edit Profile</a>
+        </li>
+	</ul>
+</nav>
