51bd8d5ac93dc0836b377bd76abdc72ec4696faf
QX <tanq0046@e.ntu.edu.sg>
2024-09-20 08:54:35 +0800

    reset

--- frontend/.svelte-kit/generated/client/nodes/4.js
+++
@@ -1,1 +0,0 @@
-export { default as component } from "../../../../src/routes/home/user-management/+page.svelte";
--- frontend/src/lib/stores.js
+++
@@ -1,5 +0,0 @@
-import { writable } from 'svelte/store';
-
-export const authStore = writable();
-
-export const userStore = writable('');
