7285f1b8b4b477fded2d3df7b0363b25a499e05a
QX <tanq0046@e.ntu.edu.sg>
2024-09-30 11:32:22 +0800

    reverted back to connection

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,36 +1,26 @@
 const mysql = require("mysql2");
 const dotenv = require("dotenv");
-const util = require("util");
 dotenv.config();
 
-
-const pool = mysql.createPool({
+const connection = mysql.createConnection({
   host: process.env.Host,
   user: process.env.User,
   password: process.env.password,
   database: process.env.database,
-  connectionLimit: 20,
-  maxIdle: 20,
-  idleTimeout: 60000,
-  queueLimit: 0,
-  enableKeepAlive: true,
-  keepAliveInitialDelay: 0,
 });
 
-
-pool.getConnection((err, connection) => {
+connection.connect((err) => {
   if (err) {
     console.error("Error connecting to database", err.stack);
     return;
   }
   console.log("Connected to the database as id" + connection.threadId);
-  connection.release(); //release the connection
 });
 
 // promisify query
 const query = (sql, params) => {
   return new Promise((resolve, reject) => {
-    pool.query(sql, params, (error, results) => {
+    connection.query(sql, params, (error, results) => {
       if (error) {
         reject(error);
       } else {
